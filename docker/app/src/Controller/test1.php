e7bbf13d38aaeeaf0a9b283f5095e2125550b97c
Matej <grubisic401@gmail.com>
2022-07-14 12:36:56 +0200

    drugi commit

--- docker/app/src/Controller/test1.php
+++ docker/app/src/Controller/test1.php
@@ -1,21 +1,72 @@
 <?php
+
 namespace App\Controller;
 
-
+use Error;
+use Google\Cloud\Firestore\FirestoreClient;
 use Symfony\Component\HttpFoundation\Response;
 
 class test1
 {
-    public function number(): Response
+    /**
+     * @Route("/test1/{projectid}", name="app_lucky_number")
+     */
+    public function firestoredata(): Response
     {
-        $number = 3;
-        
+        $projectId = 'matejgrubisicgiftcard';
+
+        $fieldnum = random_int(1000000, 9999999);
+
+
+
+        function Randomstring()
+        {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+            for ($i = 0; $i < 10; $i++) {
+                $randomString .= $characters[rand(0, $charactersLength - 1)];
+            }
+            return $randomString;
+        }
+
+        $randomid = Randomstring();
+
+
+        $firestore = new FirestoreClient([
+            'projectId' => $projectId,
+        ]);
+
+        $data = [
+            'broj' => $fieldnum
+        ];
+        $num = 0;
+        $docId = [];
+        $idRef = $firestore->collection('giftcard1');
+        $snapshot = $idRef->documents();
+        foreach ($snapshot as $id) {
+            $docId[$num] = $id->id();
+            # printf($docId[$num] . " "); << print $docId
+            $num++;
+        };
+
+        $doclength = count($docId);
+        /* testira san nesto neradi ovo 
+        for ($i = 0; $i < $doclength; $i++) {
+            for ($i2 = 0; $i2 < $doclength; $i2++) {
+                if ($docId[$i] == $docId[$i2]) {
+                    $randomid = Randomstring();
+                    $docId[$i] = $randomid;
+                }
+            };
+            printf($docId[$i] . "radi" . " ");
+        };
+        */
+        $firestore->collection('giftcard1')->document($randomid)->set($data);
+
 
         return new Response(
-            '<html><body>broj: '.$number. " " . $firestoreClient'</body></html>'
+            '<html><body>field number: ' . $fieldnum . " " . 'uspilo je' . '</body></html>'
         );
     }
 }
-
-
-?>
