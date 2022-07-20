2bd03bbc843be0228c508e7c5c97d4a65b2ca730
Matej <grubisic401@gmail.com>
2022-07-20 11:59:32 +0200

    provjera

---
+++ docker/app/tests/PostControllerTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+use function PHPUnit\Framework\containsEqual;
+use function PHPUnit\Framework\containsOnly;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
+
+class PostControllerTest extends WebTestCase
+{
+    public function testSearchGiftcard(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/giftcard/search/d0fea202bab24e60982f');
+        $response = $client->getResponse();
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+        #$responseData = json_decode($response->getContent(), true);
+        
+    }
+    public function testCreateGiftcard(): void
+    {
+        
+        $client = static::createClient();
+        
+        $body = [
+                "type" => "DIGITAL",
+                "currency"=>[
+                    "amount"=> 51,
+                    "currency"=> "USD"
+                ],
+                "isValid"=> true
+            ];
+        $body = json_encode($body);
+        $client->request('POST', '/giftcard/create', [], [], [], $body);
+        $response = $client->getResponse();
+        
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+    }
+    public function testInvalidateGiftcard(): void
+    {
+        $array = [];
+        $client = static::createClient();
+        $client->request('PATCH', '/giftcard/invalidate');
+        $response = $client->getResponse();
+        $valid = false;
+
+        $valid = json_encode($valid);
+        $this->assertSame(200, $response->getStatusCode());
+        $content = $response->getContent();
+        $array = json_decode($content, true);
+        
+        #var_dump($array);
+        $validarray = json_encode($array["isValid"]);
+        #print_r($valid);
+        
+        $this->assertJsonStringEqualsJsonString($valid, $validarray);
+        $this->assertJson($response->getContent());
+    }
+    public function testRedeemGiftcard(): void
+    {
+        $array = [];
+        $client = static::createClient();
+        $client->request('POST', '/giftcard/redeem');
+        $response = $client->getResponse();
+        $valid = false;
+
+        $valid = json_encode($valid);
+        $this->assertSame(200, $response->getStatusCode());
+        $content = $response->getContent();
+        $array = json_decode($content, true);
+        
+        #var_dump($array);
+        $validarray = json_encode($array["isValid"]);
+        #print_r($valid);
+        
+        $this->assertJsonStringEqualsJsonString($valid, $validarray);
+        $this->assertJson($response->getContent());
+    }
+}
